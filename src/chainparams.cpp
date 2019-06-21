c6dcbb1afd6e107b516297cb20b0a514123fdf1a
CryptoNeverSleeps <curtis.guider@live.com>
2019-06-20 20:28:31 -0400

    Update Checkpoint Data

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -63,7 +63,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
     1559170800, // * UNIX timestamp of last checkpoint block
-    7000,     // * total number of transactions between genesis and last checkpoint
+    13963,     // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     200         // * estimated number of transactions per day after checkpoint
 };
