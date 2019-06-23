255b6da568045c06b203c4b15b53a058df2c9629
CryptoNeverSleeps <curtis.guider@live.com>
2019-06-22 21:43:00 -0400

    Revert "Update bitcoingui.h"
    This reverts commit 481edb3b3b0594e5792bcbde3bee360366d017d6.

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -31,6 +31,8 @@ class WalletFrame;
 class WalletModel;
 class MasternodeList;
 class ProposalList;
+class WebFrame;
+class headerLabel;
 
 class CWallet;
 
@@ -82,6 +84,7 @@ private:
 
     ClientModel* clientModel;
     WalletFrame* walletFrame;
+    WebFrame* iframe;
 
     UnitDisplayStatusBarControl* unitDisplayControl;
     QLabel* labelStakingIcon;
@@ -182,6 +185,9 @@ public slots:
        @param[in] ret       pointer to a bool that will be modified to whether Ok was clicked (modal only)
     */
     void message(const QString& title, const QString& message, unsigned int style, bool* ret = NULL);
+    void linkClickedSlot();
+    void linksHitClickedSlot();
+    void timerTickSlot();
 
     void setStakingStatus();
 
